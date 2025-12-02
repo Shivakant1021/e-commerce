94bbe861c40be4a15ed28be245693c240b5cc198
Shivakant1021 <shivakant011999@gmail.com>
2025-12-02 19:13:49 +0530

    Change for deployment

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-// import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ShopContextProvider from './Context/ShopContext';
 
+// 👉 Use HashRouter instead of BrowserRouter
+import { HashRouter } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <ShopContextProvider>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </ShopContextProvider>
-    
- 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Performance (optional)
 reportWebVitals();
